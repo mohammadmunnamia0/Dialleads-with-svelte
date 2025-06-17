ee6ce27afdb16142d4ee39734acdac8d78cdbc35
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-06-17 15:09:33 +0600

    Implement the Shadcn Ui components

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  let { children } = $props();
+  import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
+</script>
+
+
+<ModeWatcher />
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Navbar from "../sections/shared/navbar/navbar.svelte";
+
+</script>
+
+
+<main>
+<Navbar/>
+</main>
+
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  /* :global(html) {
+    background-color: theme(--color-gray-100);
+  } */
+</style>
